013bc7f77ecf1f0cd784c8edb066368295e619b0
Dolan Miu <dolan_miu@hotmail.com>
2018-05-26 23:51:59 +0100

    Fix compile error

--- @types/node_helper/index.d.ts
+++ @types/node_helper/index.d.ts
@@ -1,7 +1,7 @@
 declare module "node_helper" {
     export function create(config: {
         start(): void;
-        socketNotificationReceived<T>(notification: NotificationType, payload: T): void;
+        socketNotificationReceived<T>(notification: any, payload: T): void;
         [key: string]: {};
     }): void;
 }
