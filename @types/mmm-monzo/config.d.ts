26ea6dc5f538c0203a85f0afee18aa06e009c0db
Dolan Miu <dolan_miu@hotmail.com>
2018-05-27 23:42:51 +0100

    Add account id

--- @types/mmm-monzo/config.d.ts
+++ @types/mmm-monzo/config.d.ts
@@ -2,10 +2,12 @@ interface Config {
     clientId: string;
     clientSecret: string;
     refreshToken: string;
+    accountId: string;
 }
 
 interface UncheckedConfig {
     clientId?: string;
     clientSecret?: string;
     refreshToken?: string;
+    accountId?: string;
 }
